cdf62e1cf8409c448b0b6862b2bd48a15862c008
paruljain17 <paruljain9950@gmail.com>
2025-07-02 12:03:59 +0530

    new

--- st.py
+++ st.py
@@ -8,6 +8,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import altair as alt
+import time
 #page configuration
 st.set_page_config(
     page_title="streamlit function demo",
@@ -55,6 +56,70 @@ fig , ax =plt.subplots()
 ax.plot(df.index,df.a)
 st.pyplot(fig)
 st.divider()
+
+#widgets
+st.header("5.Widgets")
+with st.form("Input form"):
+    name= st.text_input("Enter your name")
+    age=st.number_input("Enter your age")
+    mood=st.radio("Select your mood",("happy","sad","neutral"))
+    languages = st.multiselect("select your languages",("English","French","urdu","german"))
+    sumbit = st.form_submit_button("sumbit")
+    if sumbit:
+        st.success(f"Name:{name},age:{int(age)},mood:{mood},language:{languages}")
+st.divider()
+
+
+col1,col2,col3 = st.columns(3)
+with col1:
+    name= st.text_input("Enter your name")
+    age=st.number_input("Enter your age")
+with col2:
+    mood=st.radio("Select your mood",("happy","sad","neutral"))
+    languages = st.multiselect("select your languages",("English","French","urdu","german"))
+with col3:
+    st.title("OUTPUT")
+    st.title(name)
+    st.title(age)
+    st.title(mood)
+    st.title(languages)
+st.divider()
+
+col1,col2=st.columns(2)
+with col1:
+    number = st.slider("Select a number",0,100)
+with col2:
+    colour =  st.color_picker("select a color","#37189A")
+
+st.text_area("enter your message")
+st.date_input("select a date")
+st.time_input("select a time")
+st.file_uploader("upload a file")
+st.divider()
+
+#media
+st.header("6.Media")
+st.image("https://th.bing.com/th/id/OIP.2X4KDB--Ll3XHA_qs2XwzQHaE8?w=279&h=187&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")
+st.audio("https://open.spotify.com/track/48PA5xbu9RMF48IjZJ5ICs?si=686e99c529f24aab")
+#st.video("")
+
+
+#sidebar
+st.sidebar.header("Sidebar header")
+st.sidebar.write("this is sdiebar text")
+st.sidebar.button("click me")
+option = st.sidebar.selectbox("Select an option",("Option 1","Option 2"))
+
+
+with st.container():
+    st.write("this is a container")
+with st.expander("expander header"):
+    st.write("this is an expander")
+with st.spinner("loading Data..."):
+     time.sleep(7)
+st.toast("toast message",icon="😊")
+
+st.page_link("https://www.geeksforgeeks.org/machine-learning/machine-learning-lifecycle/",label = "gfg", icon="🧐")
 
 
 
